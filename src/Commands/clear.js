06a8f42d0e15864f0d3780d99f419dc12a2ddacd
Uddin-Humaiyun-215492994 <uddin513@msn.com>
2021-09-18 01:38:00 -0400

    clear.js console logs

--- src/Commands/clear.js
+++ src/Commands/clear.js
@@ -5,7 +5,10 @@ module.exports = new Command({
     description: "Clear an amount of messages.",
     permission: "ADMINISTRATOR",
     async run(message, args, client) {
+
         const amount = args[1];
+        const user = message.author.tag;
+
         if (!amount || isNaN(amount)) {
             return message.reply(`"${amount == undefined ? "Nothing" : amount}" is not valid -> Specify an amount!`);
         }
@@ -15,8 +18,9 @@ module.exports = new Command({
         if (amountParsed < 1) { return message.reply("You must delete at least 1 message!"); }
 
         message.channel.bulkDelete(amountParsed + 1);
+        console.log(`\nclear.js: ${user} cleared ${amountParsed} messages...`);
 
-        const msg = await message.channel.send(`Cleared ${amountParsed} messages!`);
-        setTimeout(() => { msg.delete() }, 5000);
+        const msg = await message.channel.send(`Cleared **${amountParsed}** messages!`);
+        setTimeout(() => { msg.delete() }, 2500);
     }
 });
