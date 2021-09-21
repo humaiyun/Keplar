de5b6b11dfba6d6366a18e42ceb04ba2d9e5ea1c
Uddin-Humaiyun-215492994 <uddin513@msn.com>
2021-09-21 16:44:54 -0400

    relevant-urban package

--- src/Commands/ud.js
+++ src/Commands/ud.js
@@ -1,10 +1,10 @@
 /* https://www.npmjs.com/package/urban-dictionary npm install urban-dictionary */
 
 const Command = require("../Structures/Command.js");
-const ud = require('urban-dictionary');
+const ud = require('relevant-urban');
 
 module.exports = new Command({
-    name: "urban",
+    name: "ud",
     description: "Search the Urban Dictionary definition of a word.",
     permission: "SEND_MESSAGES",
     async run(message, args, client) {
