0fa79aecad10f61ba6323058d9ce740ee592c9dd
Uddin-Humaiyun-215492994 <uddin513@msn.com>
2021-09-21 16:33:43 -0400

    formatting index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 console.clear();
-console.log("\n\n\n" + new Date().toUTCString());
+console.log("\n\n\nindex.js: " + new Date().toUTCString());
 
 const Client = require("./Structures/Client.js");
 const config = require("./Data/config.json");
 const client = new Client();
 
-process.on('unhandledRejection', (err) => {
-    console.log('err' + err)
+process.on("unhandledRejection", (err) => {
+    console.log("\nindex.js: " + err);
 });
 
 client.start(config.token);
