2eaef6b1ccb5610f2dec960dac610d88a74b0b9f
Uddin-Humaiyun-215492994 <uddin513@msn.com>
2021-09-21 19:19:18 -0400

    create !ud command: search urban dictionary for definition of the string input

--- src/Commands/ud.js
+++ src/Commands/ud.js
@@ -1,7 +1,8 @@
 /* https://www.npmjs.com/package/urban-dictionary npm install urban-dictionary */
 
 const Command = require("../Structures/Command.js");
-const ud = require('relevant-urban');
+const Discord = require("discord.js");
+const urban = require('relevant-urban');
 
 module.exports = new Command({
     name: "ud",
@@ -9,6 +10,45 @@ module.exports = new Command({
     permission: "SEND_MESSAGES",
     async run(message, args, client) {
 
+        const word = args.splice(1).join(" ");
+
+        if (!word) return message.reply("You must specify a word! :laughing:");
+
+        // const definition = await urban(args[1].join(' ')).catch(err => {
+        //     message.reply(`"${args}" not found...`);
+        // });
+
+        const definition = await urban(word).catch(err => {
+            message.reply(`"${word}" not found...`);
+        })
+        const defEmbed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle(`${definition.word}`)
+            .setURL(definition.urbanURL)
+            .addFields({
+                name: ":notebook_with_decorative_cover: Description",
+                value: `${definition.definition}\n` ? `${definition.definition}\n` : "No description found...",
+                inline: false
+            }, {
+                name: ":bookmark: Example",
+                value: `${definition.example}\n` ? `${definition.example}\n` : "No example found...",
+                inline: false
+            }, {
+                name: ":writing_hand: Author",
+                value: `${definition.author}\n` ? `${definition.author}\n` : "No author found...",
+                inline: false
+            }, {
+                name: ":bar_chart: Rating",
+                value: `:thumbsup:     ${definition.thumbsUp}     |     :thumbsdown:     ${definition.thumbsDown} \n`,
+                inline: false
+            })
+            .setTimestamp();
+
+
+        const sendEmbed = () => {
+            message.channel.send({ embeds: [defEmbed] });
+        }
+        sendEmbed();
 
     }
 });
