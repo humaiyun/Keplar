30c1322eb65a35bd8c567db66ad828260f89225b
Uddin-Humaiyun-215492994 <uddin513@msn.com>
2021-09-22 10:46:24 -0400

    Complete !advice command

---
+++ src/Commands/advice.js
@@ -0,0 +1,45 @@
+/* https://api.adviceslip.com/ ADVICE SLIP JSON API */
+
+const Discord = require("discord.js");
+const Command = require("../Structures/Command.js");
+const got = require("got");
+
+module.exports = new Command({
+    name: "advice",
+    description: "Generate a random piece of advice to put you at ease, or fill you in with guilt and regret.",
+    permission: "SEND_MESSAGES",
+
+    async run(message, args, client) {
+
+        // Connect to the API and then fetch the data
+        try {
+            got('https://api.adviceslip.com/advice', { JSON: true }).then(result => {
+                const content = JSON.parse(result.body);
+
+                const advice = content.slip.advice;
+
+                const adviceEmbed = new Discord.MessageEmbed()
+                    .setColor("RANDOM")
+                    // .setTitle(redditTitle)
+                    // .setImage(redditURL)
+                    // .setURL(`https://www.reddit.com${permalink}`)
+                    // .addFields({
+                    //     name: ":snowman: Subreddit",
+                    //     value: `${subreddit}`,
+                    //     inline: true
+                    // }, {
+                    //     name: ":japanese_goblin: User",
+                    //     value: `${author}`,
+                    //     inline: true
+                    // })
+                    .setDescription(advice);
+
+                message.channel.send({ embeds: [adviceEmbed] });
+            });
+        }
+        catch (err) {
+            message.channel.send("Error: " + err + "\n@Nemo#1259");
+            console.log("\advice.js: " + err);
+        }
+    }
+});
