9db4e4963f56b934cff4b70903b9481ad00315db
Uddin-Humaiyun-215492994 <uddin513@msn.com>
2021-09-22 00:11:43 -0400

    !meme almost done: just cant figure out how to post the embed
    TypeError: this.options.embeds?.map is not a function
    
    what da hell boy

--- src/Commands/meme.js
+++ src/Commands/meme.js
@@ -1,11 +1,33 @@
+/* https://www.npmjs.com/package/got : npm install got */
+
+const Discord = require("discord.js");
 const Command = require("../Structures/Command.js");
+const got = require("got");
 
 module.exports = new Command({
     name: "meme",
-    description: "Get a random meme from reddit.",
+    description: "Get a random meme from reddit and post it in an embedded format.",
     permission: "SEND_MESSAGES",
 
     async run(message, args, client) {
 
+        const subreddit = ["memes", "dankmemes", "meirl", "me_irl"];
+
+        got(`https://www.reddit.com/r/${subreddit[Math.floor(Math.random() * subreddit.length)]}/random/.json`).then(result => {
+            let content = JSON.parse(result.body);
+            console.log(`Title: ${content[0].data.children[0].data.title}\nURL: ${content[0].data.children[0].data.url}\nMisc: 👍 ${content[0].data.children[0].data.ups} 👎 ${content[0].data.children[0].data.downs} | Comments : ${content[0].data.children[0].data.num_comments}`);
+            //console.log("meme.js: content[0]: " + content[0].data.children[0].data.title);
+
+            const post = new Discord.MessageEmbed()
+                .setTitle(`${content[0].data.children[0].data.title}`)
+                .setImage(`${content[0].data.children[0].data.url}`)
+                .setColor("RANDOM")
+                .setFooter(`👍 ${content[0].data.children[0].data.ups} | 👎 ${content[0].data.children[0].data.downs} | Comments : ${content[0].data.children[0].data.num_comments}`);
+
+
+            message.channel.send({ embeds: post });
+
+
+        });
     }
 });
