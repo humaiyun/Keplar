a1cfced523c9d7658b44c2ebddbd0fc9e106adb6
Humaiyun <uddin513@msn.com>
2021-09-25 23:54:22 -0400

    support for ETH and ADA ; created a function for efficiency

--- src/Commands/crypto.js
+++ src/Commands/crypto.js
@@ -15,6 +15,64 @@ module.exports = new Command({
         const cryptoInput = args.splice(1).join(" ");
         let coinGeckoURL = `https://api.coingecko.com/api/v3/`;
 
+        function getSpecificCoinInfo(content, coinIndex) {
+            const cryptoName = content[coinIndex].id.charAt(0).toUpperCase() + content[coinIndex].id.slice(1);
+            const cryptoSymbol = content[coinIndex].symbol.toUpperCase();
+            const cryptoImage = content[coinIndex].image;
+            const cryptoLastUpdated = content[coinIndex].last_updated;
+            const cryptoRank = content[coinIndex].market_cap_rank;
+            const cryptoPrice = new Intl.NumberFormat("en-CA", {
+                style: "currency",
+                currency: "USD"
+            }).format(content[coinIndex].current_price);
+            const cryptoHigh24 = new Intl.NumberFormat("en-CA", {
+                style: "currency",
+                currency: "USD"
+            }).format(content[coinIndex].high_24h);
+            const cryptoLow24 = new Intl.NumberFormat("en-CA", {
+                style: "currency",
+                currency: "USD"
+            }).format(content[coinIndex].low_24h);
+            const cryptoMarketCap = new Intl.NumberFormat("en-CA", {
+                style: "currency",
+                currency: "USD"
+            }).format(content[coinIndex].market_cap);
+            const cryptoAllTimeHigh = new Intl.NumberFormat("en-CA", {
+                style: "currency",
+                currency: "USD"
+            }).format(content[coinIndex].ath);
+
+            let embedArr = new Discord.MessageEmbed()
+                //.setDescription(`**Current Price**\n\`${cryptoPrice}\``)
+                .setAuthor(`${cryptoName} (${cryptoSymbol})`, cryptoImage)
+                .setFields({
+                    name: ":first_place: Rank",
+                    value: `\`${cryptoRank}\``,
+                    inline: true
+                }, {
+                    name: ":dollar: Currect Price",
+                    value: `\`${cryptoPrice}\``,
+                    inline: true
+                }, {
+                    name: "📊 All Time High",
+                    value: `\`${cryptoAllTimeHigh}\``,
+                    inline: true
+                }, {
+                    name: `:chart_with_upwards_trend: 24 Hour High`,
+                    value: `\`${cryptoHigh24}\``,
+                    inline: true
+                }, {
+                    name: ":chart_with_downwards_trend: 24 Hour Low",
+                    value: `\`${cryptoLow24}\``,
+                    inline: true
+                }, {
+                    name: ":chart: Market Cap",
+                    value: `\`${cryptoMarketCap}\``,
+                    inline: true
+                });
+            return message.channel.send({ embeds: [embedArr] })
+        }
+
         if (!cryptoInput) { // get info of the top 24 crypto's by market cap
             coinGeckoURL = `${coinGeckoURL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=24&page=1&sparkline=false`;
             got(coinGeckoURL, { JSON: true })
@@ -40,7 +98,7 @@ module.exports = new Command({
                     let embedArr = new Discord.MessageEmbed()
                         .setTitle("Top Cryptocurrency by Market Cap")
                         .setDescription(`All currency is in \`USD\` :money_with_wings: \n\n[Data Provided by CoinGecko](https://www.coingecko.com/en)\n\n`);
-                    //.setDescription("");
+
                     for (let j = 0; j < cryptoName.length; j++) {
                         embedArr.addFields({
                             name: `${j + 1}. ${cryptoName[j].charAt(0).toUpperCase() + cryptoName[j].slice(1)}`,
@@ -64,77 +122,46 @@ module.exports = new Command({
                     })
                     .then(result => {
                         const content = JSON.parse(result.body);
-
-                        const cryptoName = content[0].id.charAt(0).toUpperCase() + content[0].id.slice(1);
-                        const cryptoSymbol = content[0].symbol.toUpperCase();
-                        const cryptoImage = content[0].image;
-                        const cryptoLastUpdated = content[0].last_updated;
-                        const cryptoRank = content[0].market_cap_rank;
-                        const cryptoPrice = new Intl.NumberFormat("en-CA", {
-                            style: "currency",
-                            currency: "USD"
-                        }).format(content[0].current_price);
-                        const cryptoHigh24 = new Intl.NumberFormat("en-CA", {
-                            style: "currency",
-                            currency: "USD"
-                        }).format(content[0].high_24h);
-                        const cryptoLow24 = new Intl.NumberFormat("en-CA", {
-                            style: "currency",
-                            currency: "USD"
-                        }).format(content[0].low_24h);
-                        const cryptoMarketCap = new Intl.NumberFormat("en-CA", {
-                            style: "currency",
-                            currency: "USD"
-                        }).format(content[0].market_cap);
-                        const cryptoAllTimeHigh = new Intl.NumberFormat("en-CA", {
-                            style: "currency",
-                            currency: "USD"
-                        }).format(content[0].ath);
-
-                        let embedArr = new Discord.MessageEmbed()
-                            //.setDescription(`**Current Price**\n\`${cryptoPrice}\``)
-                            .setAuthor(`${cryptoName} (${cryptoSymbol})`, cryptoImage)
-                            .setFields({
-                                name: ":first_place: Rank",
-                                value: `\`${cryptoRank}\``,
-                                inline: true
-                            }, {
-                                name: ":dollar: Currect Price",
-                                value: `\`${cryptoPrice}\``,
-                                inline: true
-                            }, {
-                                name: "📊 All Time High",
-                                value: `\`${cryptoAllTimeHigh}\``,
-                                inline: true
-                            }, {
-                                name: `:chart_with_upwards_trend: 24 Hour High`,
-                                value: `\`${cryptoHigh24}\``,
-                                inline: true
-                            }, {
-                                name: ":chart_with_downwards_trend: 24 Hour Low",
-                                value: `\`${cryptoLow24}\``,
-                                inline: true
-                            }, {
-                                name: ":chart: Market Cap",
-                                value: `\`${cryptoMarketCap}\``,
-                                inline: true
-                            });
-                        message.channel.send({ embeds: [embedArr] });
+                        getSpecificCoinInfo(content, 0);
                     });
             }
-            // else {
-            //     // Input validation
-            //     if (!isNaN(parseInt(cryptoInput))) {
-            //         const invalidEmbed = new Discord.MessageEmbed()
-            //             .setTitle("Error")
-            //             .setColor("RED")
-            //             .setDescription(`isNaN Error: is a number`);
-            //         return message.reply({ embeds: [invalidEmbed] });
-            //     }
-            //     else {
-            //         //do something maybe check detailed info per coin
-            //     }
-            // }
+            else if (cryptoInput === "etherem" || cryptoInput === "eth" || cryptoInput === "ether") {
+                coinGeckoURL = `${coinGeckoURL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=24&page=1&sparkline=false`;
+                got(coinGeckoURL, { JSON: true })
+                    .catch((err) => {
+                        const throwEmbed = new Discord.MessageEmbed()
+                            .setAuthor("Error")
+                            .setColor("RED")
+                            .setDescription(`Error Message: \`${err}\``);
+                        message.channel.send({ embeds: [throwEmbed] });
+                    })
+                    .then(result => {
+                        const content = JSON.parse(result.body);
+                        getSpecificCoinInfo(content, 1);
+                    });
+            }
+            else if (cryptoInput === "cardano" || cryptoInput === "ada") {
+                coinGeckoURL = `${coinGeckoURL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=24&page=1&sparkline=false`;
+                got(coinGeckoURL, { JSON: true })
+                    .catch((err) => {
+                        const throwEmbed = new Discord.MessageEmbed()
+                            .setAuthor("Error")
+                            .setColor("RED")
+                            .setDescription(`Error Message: \`${err}\``);
+                        message.channel.send({ embeds: [throwEmbed] });
+                    })
+                    .then(result => {
+                        const content = JSON.parse(result.body);
+                        getSpecificCoinInfo(content, 2);
+                    });
+            }
+            else {
+                const invalidEmbed = new Discord.MessageEmbed()
+                    .setTitle("Error")
+                    .setColor("RED")
+                    .setDescription(`\`${cryptoInput}\` is an invalid name or symbol`);
+                return message.reply({ embeds: [invalidEmbed] });
+            }
         }
 
     }
