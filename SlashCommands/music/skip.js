22b7d9616fe1550c68d4b4965a4b7f4fb0c28888
Humaiyun <uddin513@msn.com>
2021-10-24 18:44:50 -0400

    added skip command

---
+++ SlashCommands/music/skip.js
@@ -0,0 +1,29 @@
+
+const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const player = require("../../client/player");
+
+module.exports = {
+    userPermissions: ["SEND_MESSAGES"],
+    ...new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription(`Skips the current track`),
+
+    run: async (client, interaction, args) => {
+
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue?.playing)
+            return interaction.followUp({
+                content: "❌  |  No music is currently being played",
+            });
+
+        await queue.skip();
+
+        return interaction.followUp({
+            embeds: [new MessageEmbed()
+                .setColor("RANDOM")
+                .setDescription(`✅  |  Skipped the current track`)]
+        });
+    }
+};
