e1a1914606620af498feb6c24d6401383ad1c5f1
Humaiyun <uddin513@msn.com>
2021-10-28 14:34:18 -0400

    added shuffle command

---
+++ SlashCommands/music/shuffle.js
@@ -0,0 +1,32 @@
+
+const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const player = require("../../client/player");
+
+module.exports = {
+    userPermissions: ["SEND_MESSAGES"],
+    ...new SlashCommandBuilder()
+        .setName("shuffle")
+        .setDescription(`Shuffle the current queue`),
+
+    run: async (client, interaction, args) => {
+
+        const queue = player.getQueue(interaction.guildId);
+        if (!queue?.playing) {
+            return interaction.followUp({
+                embeds: [new MessageEmbed()
+                    .setColor("RED")
+                    .setDescription(`❌  |  No music is currently being played`)]
+            });
+        }
+
+        await queue.shuffle();
+
+        return interaction.followUp({
+            embeds: [new MessageEmbed()
+                .setColor("RANDOM")
+                .setDescription(`✅  |  Queue has been shuffled`)]
+        });
+    }
+
+};
