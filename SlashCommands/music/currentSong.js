5d084fd9076e4f8f9e08a7b24884fc09505cfe86
Humaiyun <uddin513@msn.com>
2022-02-19 19:54:41 -0500

    update currentSong command

--- SlashCommands/music/currentSong.js
+++ SlashCommands/music/currentSong.js
@@ -1,7 +1,6 @@
 
-const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { QueryType } = require("discord-player");
 const player = require("../../client/player");
 
 module.exports = {
@@ -11,38 +10,17 @@ module.exports = {
         .setDescription(`Shows information about the current song`),
 
     run: async (client, interaction, args) => {
-
         const queue = player.getQueue(interaction.guildId);
 
         if (!queue?.playing)
             return interaction.followUp({
                 embeds: [new MessageEmbed()
                     .setColor("RED")
-                    .setDescription(`There is no music being played!`)]
+                    .setDescription(`❌  |  No music is currently being played`)]
             });
 
         const progress = queue.createProgressBar();
-        //const percent = queue.getPlayerTimestamp();
-        /*
-                return interaction.followUp({
-                    embeds: [
-                        {
-                            title: "🎶 Current Song",
-                            description: `**${queue.current.title}**`, //  (\`${percent.progress}%\`)
-                            fields: [
-                                {
-                                    name: "\u200b",
-                                    value: progress,
-                                },
-                            ],
-                            color: client.config.clientColor,
-                            footer: {
-                                text: `Queued by ${queue.current.requestedBy.tag}`,
-                            },
-                        },
-                    ],
-                });
-        */
+
         return interaction.followUp({
             embeds: [new MessageEmbed()
                 .setTitle(`🎶  ${queue.current.title}  🎶`)
