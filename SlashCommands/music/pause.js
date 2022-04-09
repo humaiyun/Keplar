fd69744fac8b594687b2fff49dc89e5e713ea92a
Humaiyun <uddin513@msn.com>
2022-04-09 00:19:01 -0400

    Fix pause command not working

--- SlashCommands/music/pause.js
+++ SlashCommands/music/pause.js
@@ -11,12 +11,23 @@ module.exports = {
 
     run: async (client, interaction, args) => {
         const queue = player.getQueue(interaction.guildId);
+
+        if (!queue?.playing) {
+            return interaction.followUp({
+                embeds: [new MessageEmbed()
+                    .setColor("RED")
+                    .setDescription(`❌  |  No music is currently being played`)]
+            });
+        }
+
         queue.setPaused(true);
+
+        console.log(`\nmusic/pause.js:25: ${interaction.user.tag} paused the current track...`);
 
         return interaction.followUp({
             embeds: [new MessageEmbed()
                 .setColor("RANDOM")
-                .setDescription(`Paused the current track!`)]
+                .setDescription(`✅  |  Paused the current track`)]
         });
     }
 };
